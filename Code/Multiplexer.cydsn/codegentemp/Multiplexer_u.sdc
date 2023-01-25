fc8a354e5fa9d3fd4adba46762ba82ffb99c02fc
vdmulato <123152721+vdmulato@users.noreply.github.com>
2023-01-24 17:41:52 -0800

    Added multiplexer code
    Multiplexer code for chip CD74HC4067 ready. In this code we use it as output.

---
+++ Code/Multiplexer.cydsn/codegentemp/Multiplexer_u.sdc
@@ -0,0 +1,3 @@
+# Component constraints for C:\Users\dmula\Documents\GitHub\DigitalCamera\Code\Multiplexer.cydsn\TopDesign\TopDesign.cysch
+# Project: C:\Users\dmula\Documents\GitHub\DigitalCamera\Code\Multiplexer.cydsn\Multiplexer.cyprj
+# Date: Wed, 25 Jan 2023 00:39:44 GMT
---
+++ Code/Multiplexer.cydsn/main_cm4.c
@@ -0,0 +1,153 @@
+/* ========================================
+ *
+ * Copyright YOUR COMPANY, THE YEAR
+ * All Rights Reserved
+ * UNPUBLISHED, LICENSED SOFTWARE.
+ *
+ * CONFIDENTIAL AND PROPRIETARY INFORMATION
+ * WHICH IS THE PROPERTY OF your company.
+ *
+ * ========================================
+*/
+#include "project.h"
+#include "stdio.h"
+// Controls the channels
+void channel(int in){
+switch (in)
+{
+    case 0:
+   Cy_GPIO_Write(S0_PORT,S0_NUM,0);
+   Cy_GPIO_Write(S1_PORT,S1_NUM,0);
+ Cy_GPIO_Write(S2_PORT,S2_NUM,0);
+ Cy_GPIO_Write(S3_PORT,S3_NUM,0);
+    break;
+    case 1:
+     Cy_GPIO_Write(S0_PORT,S0_NUM,1);
+     Cy_GPIO_Write(S1_PORT,S1_NUM,0);
+     Cy_GPIO_Write(S2_PORT,S2_NUM,0);
+    Cy_GPIO_Write(S3_PORT,S3_NUM,0);
+    break;
+     case 2:
+     Cy_GPIO_Write(S0_PORT,S0_NUM,0);
+     Cy_GPIO_Write(S1_PORT,S1_NUM,1);
+     Cy_GPIO_Write(S2_PORT,S2_NUM,0);
+    Cy_GPIO_Write(S3_PORT,S3_NUM,0);
+    break;
+     case 3:
+     Cy_GPIO_Write(S0_PORT,S0_NUM,1);
+     Cy_GPIO_Write(S1_PORT,S1_NUM,1);
+     Cy_GPIO_Write(S2_PORT,S2_NUM,0);
+    Cy_GPIO_Write(S3_PORT,S3_NUM,0);
+    break;
+     case 4:
+    Cy_GPIO_Write(S0_PORT,S0_NUM,0);
+     Cy_GPIO_Write(S1_PORT,S1_NUM,0);
+     Cy_GPIO_Write(S2_PORT,S2_NUM,1);
+    Cy_GPIO_Write(S3_PORT,S3_NUM,0);
+    break;
+     case 5:
+    Cy_GPIO_Write(S0_PORT,S0_NUM,1);
+     Cy_GPIO_Write(S1_PORT,S1_NUM,0);
+     Cy_GPIO_Write(S2_PORT,S2_NUM,1);
+    Cy_GPIO_Write(S3_PORT,S3_NUM,0);
+    break;
+     case 6:
+     Cy_GPIO_Write(S0_PORT,S0_NUM,0);
+     Cy_GPIO_Write(S1_PORT,S1_NUM,1);
+     Cy_GPIO_Write(S2_PORT,S2_NUM,1);
+    Cy_GPIO_Write(S3_PORT,S3_NUM,0);
+    break;
+     case 7:
+    Cy_GPIO_Write(S0_PORT,S0_NUM,1);
+     Cy_GPIO_Write(S1_PORT,S1_NUM,1);
+     Cy_GPIO_Write(S2_PORT,S2_NUM,1);
+    Cy_GPIO_Write(S3_PORT,S3_NUM,0);
+    break;
+     case 8:
+    Cy_GPIO_Write(S0_PORT,S0_NUM,0);
+     Cy_GPIO_Write(S1_PORT,S1_NUM,0);
+     Cy_GPIO_Write(S2_PORT,S2_NUM,0);
+    Cy_GPIO_Write(S3_PORT,S3_NUM,1);
+    break;
+     case 9:
+     Cy_GPIO_Write(S0_PORT,S0_NUM,1);
+     Cy_GPIO_Write(S1_PORT,S1_NUM,0);
+     Cy_GPIO_Write(S2_PORT,S2_NUM,0);
+   Cy_GPIO_Write(S3_PORT,S3_NUM,1);
+    break;
+     case 10:
+    Cy_GPIO_Write(S0_PORT,S0_NUM,0);
+     Cy_GPIO_Write(S1_PORT,S1_NUM,1);
+     Cy_GPIO_Write(S2_PORT,S2_NUM,0);
+    Cy_GPIO_Write(S3_PORT,S3_NUM,1);
+    break;
+     case 11:
+     Cy_GPIO_Write(S0_PORT,S0_NUM,1);
+     Cy_GPIO_Write(S1_PORT,S1_NUM,1);
+     Cy_GPIO_Write(S2_PORT,S2_NUM,0);
+    Cy_GPIO_Write(S3_PORT,S3_NUM,1);
+    break;
+     case 12:
+    Cy_GPIO_Write(S0_PORT,S0_NUM,0);
+     Cy_GPIO_Write(S1_PORT,S1_NUM,0);
+     Cy_GPIO_Write(S2_PORT,S2_NUM,1);
+   Cy_GPIO_Write(S3_PORT,S3_NUM,1);
+    break;
+     case 13:
+     Cy_GPIO_Write(S0_PORT,S0_NUM,1);
+     Cy_GPIO_Write(S1_PORT,S1_NUM,0);
+     Cy_GPIO_Write(S2_PORT,S2_NUM,1);
+    Cy_GPIO_Write(S3_PORT,S3_NUM,1);
+    break;
+     case 14:
+     Cy_GPIO_Write(S0_PORT,S0_NUM,0);
+     Cy_GPIO_Write(S1_PORT,S1_NUM,1);
+     Cy_GPIO_Write(S2_PORT,S2_NUM,1);
+   Cy_GPIO_Write(S3_PORT,S3_NUM,1);
+    break;
+     case 15:
+    Cy_GPIO_Write(S0_PORT,S0_NUM,1);
+     Cy_GPIO_Write(S1_PORT,S1_NUM,1);
+     Cy_GPIO_Write(S2_PORT,S2_NUM,1);
+  Cy_GPIO_Write(S3_PORT,S3_NUM,1);
+    break;
+}
+
+}
+
+
+
+int main(void)
+{    
+    __enable_irq(); /* Enable global interrupts. */
+    /* Place your initialization/startup code here (e.g. MyInst_Start()) */   
+    UART_Start();
+    setvbuf(stdin, NULL,_IONBF,0);  
+    //enable the multiplexer sending a signal to the enable pin this is active-low
+    Cy_GPIO_Write(Mux_en_PORT,Mux_en_NUM,0);
+    int numberin = 0;
+    int states[4];
+    
+    for(;;) {
+        
+        //Button is active-low
+        if(Cy_GPIO_Read(Button_0_PORT,Button_0_NUM)==0){          
+            channel(numberin);
+            states[0] = Cy_GPIO_ReadOut(S0_PORT,S0_NUM);
+            states[1] = Cy_GPIO_ReadOut(S1_PORT,S1_NUM),
+            states[2] = Cy_GPIO_ReadOut(S2_PORT,S2_NUM),
+            states[3] = Cy_GPIO_ReadOut(S3_PORT,S3_NUM); 
+            printf("Channel #%d, Binary = %d%d%d%d \n\r",numberin,states[3],states[2],states[1],states[0]);
+            CyDelay(200);
+            
+            if(numberin == 15){
+            numberin = 0;
+            }else{
+                numberin=numberin+1;
+            }
+            
+           } 
+
+ }
+}
+/* [] END OF FILE */
